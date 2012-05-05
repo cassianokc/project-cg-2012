5b6a4f97c82122a93c5196ce986e833a9e4e5002
Cassiano Casagrande <cassianokleinert@gmail.com>
2012-05-04 21:21:49 -0300

    nao tem mais a bolinha hehehe, um pedaco da skybox desenhada(sim, falta bastante coisa pra ta pronta), mouse tah funcionando so falta prender ele na tela(go mika go!)

---
+++ Models/SkyboxPiece.java
@@ -0,0 +1,20 @@
+
+package Models;
+
+import java.io.File;
+import java.io.IOException;
+/**
+ *
+ * @author cassianokc
+ */
+public class SkyboxPiece extends Model{
+    float x0, y0, z0;
+    float x1, y1, z1;
+    float x2, y2, z2;
+    public SkyboxPiece(File file) throws IOException{
+        super(file);
+    }
+    
+}
+
+
