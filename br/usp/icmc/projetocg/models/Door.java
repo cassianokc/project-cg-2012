474d8e3ae5f330c64d8b571fd9bdca9d375567b1
Gustavo Livrare Martins <gustavolivrare@gmail.com>
2012-05-07 17:36:21 -0300

    models - só alterei o nome do pacote

---
+++ br/usp/icmc/projetocg/models/Door.java
@@ -0,0 +1,76 @@
+package br.usp.icmc.projetocg.models;
+
+import java.io.File;
+import java.io.IOException;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+
+/**
+ *
+ * @author cassianokc
+ * 
+ */
+public class Door extends Model {
+
+    private float alpha;
+    /**
+     * Reference point to rotation vector.
+     */
+    private float xref, yref, zref;
+    /**
+     * Rotation vector.
+     */
+    private float xn, yn, zn;
+    /**
+     * Current state of door, 0 is normal, 1 is opening and 2 is closing.
+     */
+    private char doorState;
+
+
+    /**
+     * Construct a Fan object.
+     *
+     * @param file The file containing the object.
+     * @throws IOException
+     */
+    public Door(File file, float xref, float yref, float zref, 
+            float xn, float yn, float zn) throws IOException {
+        super(file);
+        this.xref = xref;
+        this.yref = yref;
+        this.zref = zref;
+        this.xn = xn;
+        this.yn = yn;
+        this.zn = zn;
+
+    }
+
+    @Override
+    public void draw(GLAutoDrawable glAutodrawable) {
+        if (doorState == 1){
+            if (alpha < 90f)
+                alpha++;
+            else doorState = 0;
+        }
+        if (doorState == 2){
+            if (alpha > 0f)
+                alpha--;
+            else doorState = 0;
+        }        
+        GL gl = glAutodrawable.getGL();
+        gl.glPushMatrix();
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glTranslatef(xref, yref, zref);
+        gl.glRotatef(alpha, xn, yn, zn);
+        super.draw(glAutodrawable);
+        gl.glPopMatrix();
+    }
+
+    public void open() {
+        this.doorState = 1;
+    }
+
+    public void close() {
+        this.doorState = 2;
+    }
+}
